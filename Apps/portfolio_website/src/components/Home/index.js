6855b4b490a12567b39b0faa87366821febd38d1
Pizzadoesntlie <32754723+Pizzadoesntlie@users.noreply.github.com>
2020-05-22 23:33:31 -0400

    todo app added

--- Apps/portfolio_website/src/components/Home/index.js
+++ Apps/portfolio_website/src/components/Home/index.js
@@ -8,9 +8,11 @@ class Home extends React.Component {
                 <h1>About Me</h1>
                 <img className="bio" src="images/bio_pic.jpg" alt="Shanice" />
                 <p className="bio">Hi! My name is Shanice Gordon. I'm a recent graduate of the University of Central Florida. 
-                I have a Bachelors in Digital Media. I like technology and I like to create. Let's make magic together... or websites, whatever floats your boat.
-                I’m really grateful that I’ve found the world of web design, it’s given me an outlet and an opportunity to 
-	            create projects that I’m proud of.</p>  
+                I have a Bachelors in Digital Media. I like technology and I like to create. 
+                I’m really grateful that I’ve found the world of web design, it’s given me an outlet and 
+                an opportunity to create projects that I’m proud of.
+                Let's make magic together... or websites, whatever floats your boat.
+                </p>  
                 <div className="footer">
                  <h3>&copy; Shanice Gordon. All Rights Reserved. 2020</h3> 
                 </div>
--- Apps/portfolio_website/src/components/Portfolio/index.js
+++ Apps/portfolio_website/src/components/Portfolio/index.js
@@ -4,8 +4,12 @@ class Portfolio extends React.Component {
     render() {
         return(
             <div className="grid-por">
+                <p style={{textAlign:"center", color: "#000000", fontWeight:"bold"}}>
+                    PROJECTS ARE CURRENTLY HOSTED ON REPL.IT</p>
                 <p><img className="w_api" src="images/weather_api.JPG" alt="cards portfolio piece" /></p>
                 <p><a href="https://repl.it/@Pizzadoesntlie/WeatherAPI">Simple Weather API</a></p>
+                <p><img className="todo_app" src="images/todo_app.JPG" alt="todolist app portfolio piece" /></p>
+                <p><a href="https://repl.it/@Pizzadoesntlie/todoapp">Simple Todo App</a></p>
                 <div className="footer">
                  <h3>&copy; Shanice Gordon. All Rights Reserved. 2020</h3> 
                 </div>
