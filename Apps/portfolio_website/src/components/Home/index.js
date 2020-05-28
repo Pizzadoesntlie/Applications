b4df6ac25d4f57f7fba65c5a483a4a31980edfec
Pizzadoesntlie <32754723+Pizzadoesntlie@users.noreply.github.com>
2020-05-28 15:40:09 -0400

    image update

--- Apps/portfolio_website/src/components/Home/index.js
+++ Apps/portfolio_website/src/components/Home/index.js
@@ -6,13 +6,15 @@ class Home extends React.Component {
         return(
              <div className="head">
                 <h1>About Me</h1>
-                <img className="bio" src="images/bio_pic.jpg" alt="Shanice" />
+                <div className="head_bio">
+                <img className="bio" src="images/shanice_2020.jpg" alt="Shanice" />
                 <p className="bio">Hi! My name is Shanice Gordon. I'm a recent graduate of the University of Central Florida. 
                 I have a Bachelors in Digital Media. I like technology and I like to create. 
                 I’m really grateful that I’ve found the world of web design, it’s given me an outlet and 
                 an opportunity to create projects that I’m proud of.
                 Let's make magic together... or websites, whatever floats your boat.
                 </p>  
+                </div>
                 <div className="footer">
                  <h3>&copy; Shanice Gordon. All Rights Reserved. 2020</h3> 
                 </div>
