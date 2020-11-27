a0ad97208fb952b6487a5006b33c3c75b5dff6ec
Shanice Gordon <switchfoot2297@gmail.com>
2020-11-27 12:51:08 -0500

    picture change

--- Apps/portfolio_website/src/components/Home/index.js
+++ Apps/portfolio_website/src/components/Home/index.js
@@ -7,12 +7,12 @@ class Home extends React.Component {
              <div className="head">
                 <h1>About Me</h1>
                 <div className="head_bio">
-                <img className="bio" src="images/shanice_2020.jpg" alt="Shanice" />
+                <img className="bio" src="images/Shanice2020.jpg" alt="Shanice" />
                 <p className="bio">Hi! My name is Shanice Gordon. I'm a recent graduate of the University of Central Florida. 
                 I have a Bachelors in Digital Media. I like technology and I like to create. 
                 I’m really grateful that I’ve found the world of web design, it’s given me an outlet and 
                 an opportunity to create projects that I’m proud of.
-                Let's make magic together... or websites, whatever floats your boat.
+                Let's make magic together... or websites, whatever floats your boat. Oh! By the way this website is running in react.
                 </p>  
                 </div>
                 <div className="footer">
